d110b75dedd1ad21ea8cb49b461b5846aa7130d0
BrenonOrtega <brenon.ortega@hotmail.com>
2024-03-08 08:19:18 +0100

    Fixes redis sorted set range by rank arguments.

--- Awarean.BrayaOrtega.RinhaBackend.Q124/Infra/CachedRepository.cs
+++ Awarean.BrayaOrtega.RinhaBackend.Q124/Infra/CachedRepository.cs
@@ -35,7 +35,7 @@ public sealed class CachedRepository : ICachedRepository
 
     private static async Task<Account> GetAccountByIdCore(int id, IDatabase db)
     {
-        var transactions = db.SortedSetRangeByRankWithScores(GetBankStatementKey(id), order: Order.Descending, take: 1);
+        var transactions = db.SortedSetRangeByRankWithScores(GetBankStatementKey(id), order: Order.Descending, start:0, stop: 1);
 
         var lastTransaction = transactions.FirstOrDefault();
         if (string.IsNullOrEmpty(lastTransaction.Element))
